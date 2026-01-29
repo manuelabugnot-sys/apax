4ae1f7b29564e60f5514698b95b35f41154bab11
manuelabugnot-sys <manuelabugnot@gmail.com>
2026-01-29 13:54:10 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx'; // Asegurate de agregar el .tsx aquí si no usás compilador
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("No se pudo encontrar el elemento root para montar la app");
 }
 
 const root = ReactDOM.createRoot(rootElement);
