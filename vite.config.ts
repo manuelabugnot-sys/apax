fce0d75fbc05e9b97e2f12dade2eae192e588ffe
manuelabugnot-sys <manuelabugnot@gmail.com>
2026-01-30 18:36:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd()), // Esto reemplaza al __dirname que daba error
       }
-    };
+    },
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    }
+  };
 });
